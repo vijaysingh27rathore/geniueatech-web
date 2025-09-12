434f6045ad46e581fc0d5d2e3e649608e14d0279
vijaysingh27rathore <vijaysingh27rathore@gmail.com>
2025-09-12 18:43:46 +0530

    commit
    commit

---
+++ .next/server/middleware-build-manifest.js
@@ -0,0 +1,33 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": [
+      "static/chunks/e60ef129113f6e24.js",
+      "static/chunks/fe5a9ad2b1eda255.js",
+      "static/chunks/turbopack-54f4260c2c9c39fa.js"
+    ],
+    "/_error": [
+      "static/chunks/17722e3ac4e00587.js",
+      "static/chunks/fe5a9ad2b1eda255.js",
+      "static/chunks/turbopack-559032ee9d630090.js"
+    ]
+  },
+  "devFiles": [],
+  "ampDevFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/c62b4372c26b460f.js",
+    "static/chunks/89fec6e6de8a2578.js",
+    "static/chunks/7880f8283a6f3daf.js",
+    "static/chunks/8082ab48faca5ea1.js",
+    "static/chunks/turbopack-9f2123101fb1106c.js"
+  ],
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VN8Fw1RXFR8cwGu6729RDYHLbqFGBvaOz/ixqfdxy8A=\"\n}"
